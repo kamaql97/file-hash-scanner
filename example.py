c4dbcd11c76b3ccc7155a8349c8fe1cfbd3a20f4
Kamalq97 <45042524+kamalq97@users.noreply.github.com>
2020-12-18 16:14:27 +0200

    Added example

---
+++ example.py
@@ -0,0 +1,8 @@
+from solution.scanner import FileScanner
+
+
+if __name__ == "__main__":
+    """ Example usage code """
+
+    f = FileScanner('84c82835a5d21bbcf75a61706d8ab549')    #('E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855')
+    f.request_data()
--- solution/scanner.py
+++ solution/scanner.py
@@ -88,11 +88,3 @@ class FileScanner:
             make_md_table('scanned file', hashes)
             make_md_table('results', resuls)
             make_md_table('scans', scans)
-
-
-
-if __name__ == "__main__":
-    """ Example usage code """
-
-    f = FileScanner('84c82835a5d21bbcf75a61706d8ab549')    #('E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855')
-    f.request_data()
