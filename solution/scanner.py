1566372cfcf0809492ab4d12101f3c7dca1cb9b8
Kamalq97 <45042524+kamalq97@users.noreply.github.com>
2020-12-20 15:10:14 +0200

    Update scanner.py

--- solution/scanner.py
+++ solution/scanner.py
@@ -18,8 +18,7 @@ from solution.exceptions import (InvalidFileHashError, APIKeyNotFoundError,
 def request_data(resource=None, user_key=None):
     '''
     Method that configures error logging, checks for exisitance of API key
-    and a valid file hash, then makes a get request and returns the JSON
-    reponse as a string in the markdown format
+    and a valid file hash, then makes a get request to the VirusTotal Service
 
     Inputs:
         resource --- String representing a file's hash
@@ -60,30 +59,43 @@ def request_data(resource=None, user_key=None):
         logging.error(err)
         raise Exception('An unknown error has occured')
     else:
-        try:
-            data = response.json()
-            msg = data['verbose_msg']
+        return handle_data(response.json())
 
-            if data['response_code'] == 1:      # item present and could be retrieved
 
-                hashes = [{'MD5': data['md5'], 'SHA-1': data['sha1'], 'SHA-256': data['sha256']}]
-                resuls = [{'Total Scans': data['total'], 'Positive Scans': data['positives']}]
-                scans = [{'Scan Origin': key, 'Scan Result': val['detected']}
-                        for key, val in data['scans'].items()]
+def handle_data(data):
+    '''
+    Method that attemps to parse the JSON response returned by the service
 
-                out_str = (make_md_table('scanned file', hashes)
-                            + make_md_table('results', resuls)
-                            + make_md_table('scans', scans))
+    Inputs:
+        data --- reponse JSON to be decoded 
 
-        except ValueError as err:
-            logging.error(err)
-            raise
-        except KeyError as err:
-            logging.error(err)
-            raise
-        except Exception as err:
-            logging.error(err)
-            raise Exception('An unknown error has occured')
-        else:
-            print (msg)
-            return out_str
+    Output:
+        String containg the scan results (if the file could be scanned)
+    '''
+    
+    try:
+        msg = data['verbose_msg']
+
+        if data['response_code'] == 1:      # item present and could be retrieved
+
+            hashes = [{'MD5': data['md5'], 'SHA-1': data['sha1'], 'SHA-256': data['sha256']}]
+            resuls = [{'Total Scans': data['total'], 'Positive Scans': data['positives']}]
+            scans = [{'Scan Origin': key, 'Scan Result': val['detected']}
+                    for key, val in data['scans'].items()]
+
+            out_str = (make_md_table('scanned file', hashes)
+                        + make_md_table('results', resuls)
+                        + make_md_table('scans', scans))
+
+    except ValueError as err:
+        logging.error(err)
+        raise
+    except KeyError as err:
+        logging.error(err)
+        raise
+    except Exception as err:
+        logging.error(err)
+        raise Exception('An unknown error has occured')
+    else:
+        print (msg)
+        return out_str
