8d8fa052c9acc32576b3aa8ff2ee025b8ad93ede
Kamalq97 <45042524+kamalq97@users.noreply.github.com>
2020-12-20 14:02:09 +0200

    Update example.py

--- example.py
+++ example.py
@@ -4,8 +4,7 @@ Palo Alto Networks Assignement - Kamal Qarain
 Example usage code
 """
 
-from solution.scanner import FileScanner
+from solution.scanner import request_data
 
-
-f = FileScanner('84c82835a5d21bbcf75a61706d8ab549')
-f.request_data()
+with open('output.md', 'w+') as output_file:
+    output_file.write(request_data('6E0B782A9B06834290B24C91C80B12D7AD3C3133'))
