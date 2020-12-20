34c62c0a47ddeb65c4848a43e9e8f6e9c789e9d3
Kamalq97 <45042524+kamalq97@users.noreply.github.com>
2020-12-20 14:55:06 +0200

    Update scanner.py

--- solution/scanner.py
+++ solution/scanner.py
@@ -5,7 +5,6 @@ File scanning code
 """
 
 import logging
-import sys
 import os
 
 import requests
@@ -16,23 +15,30 @@ from solution.exceptions import (InvalidFileHashError, APIKeyNotFoundError,
                                 VirusTotalUnreachableError, UnexpectedResponseError)
 
 
-def request_data(resource=None):
+def request_data(resource=None, user_key=None):
     '''
     Method that configures error logging, checks for exisitance of API key
-    and a valid file hash, then makes a get request and returns the JSON reponse
-    as a string in the markdown format
+    and a valid file hash, then makes a get request and returns the JSON
+    reponse as a string in the markdown format
+
+    Inputs:
+        resource --- String representing a file's hash
+        user_key --- Optional string; VirusTotal API key (if not in environmental variables)
+
+    Output:
+        String containg the scan results (if the file could be scanned)
     '''
 
     logging.basicConfig(filename='errors.log', level=logging.ERROR,
                         format='%(asctime)s.%(msecs)03d %(levelname)s: %(funcName)s: %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
-    
+
     out_str = ''
 
     if resource is None or not is_valid_hash(resource):
         raise InvalidFileHashError()
 
-    api_key = os.environ.get('VIRUSTOTAL_API_KEY')
+    api_key = os.environ.get('VIRUSTOTAL_API_KEY', user_key)
     if api_key is None:
         raise APIKeyNotFoundError()
 
@@ -56,9 +62,9 @@ def request_data(resource=None):
     else:
         try:
             data = response.json()
-              
-            if data['response_code'] == 1:  # item present and could be retrieved
-                print(data['verbose_msg'])
+            msg = data['verbose_msg']
+
+            if data['response_code'] == 1:      # item present and could be retrieved
 
                 hashes = [{'MD5': data['md5'], 'SHA-1': data['sha1'], 'SHA-256': data['sha256']}]
                 resuls = [{'Total Scans': data['total'], 'Positive Scans': data['positives']}]
@@ -68,15 +74,16 @@ def request_data(resource=None):
                 out_str = (make_md_table('scanned file', hashes)
                             + make_md_table('results', resuls)
                             + make_md_table('scans', scans))
-            else:               # not in database [0] or still queued for analysis [-2]
-                sys.exit(data['verbose_msg'])
+
         except ValueError as err:
             logging.error(err)
-            raise 
+            raise
         except KeyError as err:
             logging.error(err)
-            raise 
+            raise
         except Exception as err:
             logging.error(err)
             raise Exception('An unknown error has occured')
-        return out_str
+        else:
+            print (msg)
+            return out_str
